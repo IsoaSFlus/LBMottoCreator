8d97468b56aa67931520d0228a30fbfc1ef682a1
IsoaSFlus <me@isoasflus.me>
2016-04-13 20:23:45 +0800

    nearly done

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,38 +10,50 @@ MainWindow::~MainWindow()
     delete ui;
     delete img;
     img_b = NULL;
-
+    sub = NULL;
 }
 
-void picRender()
+void MainWindow::picRender()
 {
     QString temp;
-    temp = all + "Style: Default," + font +",54,&H00FFFFFF,&H00FFFFFF,&H00295479,&H00FFFFFF,-1,0,0,0,100,100,3,0,1,2,0,5,10,10,10,1" + "\n" + "[Events]" + "Dialogue: 0,0:00:00.00,0:00:06.00,Default,,0000,0000,0000,,{\\an5\\fs"
-}
-
-void MainWindow::on_lineEdit_2_textEdited(int num)
-{
-
     QImage img_temp = img->copy();
 
-    siteA = 321 - num;
-    QString temp;
-    temp = all + sectA + QString("%1").arg(num) + sectB + "\n";
+    temp = all + "Style: Default," + font +",54,&H00FFFFFF,&H00FFFFFF,&H00295479,&H00FFFFFF,-1,0,0,0,100,100,3,0,1,2,0,5,10,10,10,1" + QString("\n");
+    temp += "[Events]" + QString("\n");
+    temp += QString("Dialogue: 0,0:00:00.00,0:00:06.00,Default,,0000,0000,0000,,{\\an5\\fs") + QString("%1").arg(fsA) + QString("\\blur3\\pos(500,") + QString("%1").arg(siteA) + ")}" + motto + QString("\n");
+    temp += QString("Dialogue: 0,0:00:00.00,0:00:06.00,Default,,0000,0000,0000,,{\\an5\\fs") + QString("%1").arg(fsB) + QString("\\blur3\\pos(") + QString("%1").arg(siteB_h) + "," +  QString("%1").arg(siteB) + QString(")}") + name + QString("\n");
+  //  temp += QString("Dialogue: 0,0:00:00.00,0:00:06.00,Default,,0000,0000,0000,,{\\an5\fs") + QString("%1").arg(fsB) + QString("\blur3\\pos(") + QString("%1").arg(siteB) + "," +  QString("%1").arg(siteB_h) + QString(")}") + name;
+    qDebug()<<temp;
     const std::string str = temp.toStdString();
-    //qDebug()<<temp;
     strcpy(sub,str.c_str());
 
     img_b = img_temp.bits();
 
-    lb_render(img_b, sub, all.length()+300);
+    lb_render(img_b, sub, all.length()+1000);
     ui->label->setPixmap(QPixmap::fromImage(img_temp));
+}
+void MainWindow::siteAChanged(int num)
+{
+    siteA = 321 - num;
+    picRender();
+}
+
+void MainWindow::siteBChanged(int num)
+{
+    siteB = 321 - num;
+    picRender();
+}
+void MainWindow::siteBhChanged(int num)
+{
+    siteB_h = num;
+    picRender();
 }
 
 void MainWindow::showFont()
 {
     bool ok;
     QFont f = QFontDialog::getFont(&ok);
-    f.family();
+    font = f.family();
 }
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -51,23 +63,34 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     ui->verticalSlider->setRange(0,321);
     ui->verticalSlider_2->setRange(0,321);
+    ui->horizontalSlider->setRange(0,1040);
+
+    ui->verticalSlider->setValue(321-140);
+    ui->verticalSlider_2->setValue(321-250);
+    ui->horizontalSlider->setValue(800);
+
     QFile file("/home/midorikawa/qt/untitled5/lb.ass");
     file.open(QIODevice::ReadOnly | QIODevice::Text);
     QTextStream in_stream(&file);
     all = in_stream.readAll();
-    sub = (char*)malloc(all.length()+300);
+    sub = (char*)malloc(all.length()+1000);
     //in_stream.flush();
     file.close();
 
     img = new QImage("/home/midorikawa/qt/untitled5/EF_WR_BG00.png");
     img_b = img->bits();
+    siteA = 140;
+    siteB = 250;
+    siteB_h = 800;
+    fsA = 55;
+    fsB = 25;
+    motto = "看来交易已经成功了";
+    name = "Ludwig Van Beethoven";
 
-
-    //lb_render(img_b, sub, all.length()+300);
-//    free(sub);
-  //  qDebug()<<"110";
     ui->label->setPixmap(QPixmap::fromImage(*img));
-
-    connect(ui->verticalSlider, &QSlider::valueChanged, this, &MainWindow::on_lineEdit_2_textEdited);
+    picRender();
+    connect(ui->verticalSlider, &QSlider::valueChanged, this, &MainWindow::siteAChanged);
+    connect(ui->verticalSlider_2, &QSlider::valueChanged, this, &MainWindow::siteBChanged);
+    connect(ui->horizontalSlider, &QSlider::valueChanged, this, &MainWindow::siteBhChanged);
     connect(ui->pushButton_3, &QPushButton::clicked, this, &MainWindow::showFont);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -17,18 +17,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    void picRender(int siteA, int siteB, int fsA, int fsB, int siteB_h, QString font, QString motto, QString name);
+    void picRender();
 
 private slots:
-    void on_lineEdit_2_textEdited(int num);
+    void siteAChanged(int num);
+    void siteBChanged(int num);
+    void siteBhChanged(int num);
     void showFont();
 
 private:
     Ui::MainWindow *ui;
     QImage *img;
     uchar *img_b;
-    const QString sectA = "Dialogue: 0,0:00:00.00,0:00:06.00,Default,,0000,0000,0000,,{\\an5\\fs37\\blur3\\pos(500,";
-    const QString sectB = ")}你为什么这么熟练啊";
     char *sub;
     QString all;
     int siteA, siteB, fsA, fsB, siteB_h;
